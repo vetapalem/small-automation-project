306b046c39dbd9df226d1aed0c3679a8a829e5c2
vetapalem <99022630+vetapalem@users.noreply.github.com>
2023-03-16 19:00:41 +0530

    driver fixtures and report generation
    fully customized reports

--- test_data/conftest.py
+++ test_data/conftest.py
@@ -1,49 +1,49 @@
-from selenium import webdriver as wb
-from webdriver_manager.chrome import ChromeDriverManager as ch
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager as ec
-from selenium.webdriver.edge.service import Service
-import pytest as pyt
-from sample_project.utilities_files.make_log import login_er as er
-from sample_project.page_object_model import login as bn
+from selenium import webdriver as wb #webdriver object
+from webdriver_manager.chrome import ChromeDriverManager as ch #chrome driver object
+from selenium.webdriver.chrome.service import Service #service object for chrome
+from webdriver_manager.microsoft import EdgeChromiumDriverManager as ec # edge driver object
+from selenium.webdriver.edge.service import Service #service object for edge
+import pytest as pyt #pytest object
+from sample_project.utilities_files.make_log import login_er as er #logging file object
+from sample_project.page_object_model import login as bn #source code may not
 
 
-
+# making fixture by pytest side return driver object
 @pyt.fixture
 def link(browser):
     mm=er()
     if browser == 'edge':
         if driver is None:
         
-            mm.info('edge driver is successfully working ')
-            driver=wb.Edge(service=Service(ec().install()))
+            mm.info('edge driver is successfully working ')#loggin by driver side
+            driver=wb.Edge(service=Service(ec().install())) #edge driver
             return driver
     
     else:
-            mm.info('chrome driver is successfully working')
+            mm.info('chrome driver is successfully working')#loggin by driver side
             
-            driver=wb.Chrome(service=Service(ch().install()))
+            driver=wb.Chrome(service=Service(ch().install())) #chrome driver
             return driver
 
 
 
 
 
-def pytest_html_report_title(report):
-    report.title = "shopping site...."
+def pytest_html_report_title(report):# report titile generation
+    report.title = "shopping site...." #report title
 
-def pytest_addoption(parser):    #this will get the value from cli/hooks
+def pytest_addoption(parser):    #this will get the value from cli/hooks (pytest side hook generation)
     parser.addoption('--browser')
 
 
-@pyt.fixture
+@pyt.fixture# this is used for cmd output console | execution options
 def browser(request):
     return request.config.getoption("--browser")
 
 ########### pytest HTML Report ################
 
 # It is hook for Adding Environment info to HTML Report
-def pytest_configure(config):
+def pytest_configure(config):#table data changing area
     config._metadata['Project Name'] = 'nop Commerce'
     config._metadata['Module Name'] = 'Customers'
     config._metadata['Tester'] = 'jinga'
@@ -56,7 +56,7 @@ def pytest_metadata(metadata):
 
 
 
-@pyt.hookimpl(hookwrapper=True)
+@pyt.hookimpl(hookwrapper=True) #adding screenshots and other things...
 def pytest_runtest_makereport(item, call):
     pytest_html = item.config.pluginmanager.getplugin("html")
     outcome = yield
