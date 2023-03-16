9722ec4e82943fd8602e6d4cfbfd491fa6c82792
vetapalem <99022630+vetapalem@users.noreply.github.com>
2023-03-16 18:48:39 +0530

    creating log file

--- utilities_files/make_log.py
+++ utilities_files/make_log.py
@@ -1,13 +1,13 @@
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(r'D:\testing_pro1\project_level\sample_project\logs\sample_pro.log')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%d/%m/%Y %I:%M:%S %p')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+import logging #logging module
+logger = logging.getLogger(__name__)# root
+logger.setLevel(logging.DEBUG) #log level
+file_handler = logging.FileHandler(r'..\sample_pro.log') #log file 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%d/%m/%Y %I:%M:%S %p') #log formate 
+file_handler.setFormatter(formatter) #log file handler
+logger.addHandler(file_handler) #log addhandler 
 def login_er():
 
-    return logger
+    return logger #return logger object
     # logger.debug('Debug message')
     # logger.info('Info message')
     # logger.warning('Warning message')
